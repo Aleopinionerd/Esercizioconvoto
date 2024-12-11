99d4a406450cdae8075725b6523c0cbbe59645d2
Alessandra <alexandra.zaccaria46@gmail.com>
2024-12-11 09:57:50 +0100

    Primo commit

---
+++ Esercizioconvoto/src/registrostudenti/Studenti.java
@@ -0,0 +1,24 @@
+package registrostudenti;
+
+public class Studenti {
+	/*Gestione di un Registro di Studenti
+	Creare un programma Java che consenta di gestire un registro degli studenti. Il programma deve:
+
+	Registrare nuovi studenti con:
+	Nome,
+	Cognome,
+	Matricola (univoca).
+	Visualizzare l'elenco degli studenti.
+	Cercare uno studente tramite la matricola.
+	Requisiti Tecnici:
+	Creare una classe Studente con i campi necessari.
+	Utilizzare un ArrayList per memorizzare gli studenti.
+	Creare un menu testuale che permetta all'utente di:
+
+	Aggiungere uno studente.
+	Visualizzare tutti gli studenti.
+	Cercare uno studente per matricola.
+	Uscire dal programma
+	*/
+
+}
